e1c1bf8e43e02f4117439f5e0bfcdf1f1186d751
tsmay1 <againstonine@gmail.com>
2025-05-31 16:04:17 -0400

    created a window

--- main.py
+++ main.py
@@ -2,6 +2,15 @@ import pygame
 from constants import *
 
 def main():
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+        screen.fill((0,0,0))
+        pygame.display.flip()
+
     print("Starting Asteroids!")
     print("Screen width: 1280")
     print("Screen height: 720")
